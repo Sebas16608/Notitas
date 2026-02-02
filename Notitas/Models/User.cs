c285cde89ae13be2e436e91d164c0578567b029f
Sebas16608 <asebasrr444@gmail.com>
2026-02-01 20:48:16 -0600

    chore: Creating the IsActive field

--- Notitas/Models/User.cs
+++ Notitas/Models/User.cs
@@ -6,6 +6,7 @@ namespace Users.Model
         public required string Name { get; set; }
         public required string Email { get; set; }
         public required string PasswordHash { get; set; }
+        public bool IsActive { get; set; }
 
         public List<Notitas.Model.Notita> Notitas { get; set; } = new();
     }
