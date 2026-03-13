f58975993b875e8cd21473bf62451d845a8c8644
Sebas16608 <asebasrr444@gmail.com>
2026-03-12 22:50:14 -0600

    feat: Modelo de notitas

---
+++ backend/src/models/notitas.models.ts
@@ -0,0 +1,45 @@
+import { DataTypes, Model } from "sequelize";
+import sequelize from "../config/database";
+
+class Notita extends Model{
+    declare id? : number;
+    declare title: string;
+    declare content: string;
+    declare userId: string;
+}
+
+Notita.init({
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+    },
+    title: {
+        type: DataTypes.STRING(255),
+        allowNull: true,
+        validate: {
+            len: [1,255]
+        },
+    },
+
+    content: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    userId: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        references: {
+            model: "User",
+            key: "id",
+        },
+        onDelete: "CASCADE",
+        onUpdate: "CASCADE",
+    },
+},
+{
+    sequelize,
+    modelName: "Notita",
+    tableName: "Notitas",
+}
+);
